99712c7047333abcc798f0d4df93bca328c8fcd9
Syed Musa Raza Zaidi <m4musa110@gmail.com>
2026-05-22 22:22:30 +0500

    Removing Everthing

--- MediatR/CustomMediatR/IMediator.cs
+++
@@ -1,6 +0,0 @@
-namespace TheMediatR;
-
-/// <summary>
-/// Defines a mediator to encapsulate request/response and publishing interaction patterns.
-/// </summary>
-public interface IMediator : ISender, IPublisher;
